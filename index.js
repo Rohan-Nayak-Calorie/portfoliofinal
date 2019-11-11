954ede06bd49aabb8116a2dd2a7e69da63d3caef
Rohan Nayak <rohannayak97@gmail.com>
2019-11-11 09:04:07 -0800

    final

---
+++ index.js
@@ -0,0 +1,17 @@
+document.querySelector("#caloriebutton").addEventListener("click",onclick);
+function onclick(){
+    location.href = "./tera.html"
+    console.log("caloriebutton")
+}
+document.querySelector("#terabutton").addEventListener("click",oneclick);
+function oneclick(){
+    location.href = "./calorie.html"
+    console.log("terabutton")
+}
+
+document.querySelector("#youtubebutton").addEventListener("click",onnclick);
+function onnclick(){
+    location.href = "https://www.youtube.com/channel/UCEQjWWhCp1GUmgwtlaj0DKg"
+    console.log("ytbutton")
+}
+
